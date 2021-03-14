bbdbd825bdc553ac3bba7c06e0d28c4d13b7200e
Krzysztof <kpiekorz919@protonmail.com>
2021-03-15 00:03:21 +0100

    pthred add

--- main.c
+++ main.c
@@ -2,11 +2,33 @@
 // #include <conio.h>
 #include "accelerometer_manager.h"
 #include "auto_model.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static void main_PrintArgs(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
-    AutoModel_ModelState();
+    // main_PrintArgs(argc, argv);
+    // AutoModel_ModelState();
 
+    // create basic pthread app
+	pid_t pid;
+
+	printf("I'm a child process talking to you from another program, pid = %d\n", getpid());
+	printf("I'm a child process talking to you from another program, my parent pid = %d\n", getppid());
+
+	printf("Type 'q' to quit both processes.\n");
+	while(getc(stdin) == 'q') {}
+
+	return EXIT_SUCCESS;
+}
+
+static void main_PrintArgs(int argc, char *argv[])
+{
     int i;
     if( argc >= 2 )
     {
@@ -18,7 +40,6 @@ int main(int argc, char *argv[])
     }
     else
     {
-        printf("argument list is empty.\n");
+        printf("Argument list is empty!\n");
     }
-    return 0;
 }
