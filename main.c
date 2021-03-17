e3c566eb4a870af197c323eca1683a0790533654
Krzysztof <kpiekorz919@protonmail.com>
2021-03-18 00:11:44 +0100

    modules = process

--- main.c
+++ main.c
@@ -7,15 +7,43 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+/* modules init functions */
+typedef void (*module_init)(void);
+
+typedef struct
+{
+    module_init module_init_func;
+} module_init_t;
+
+const module_init_t system_module_init[] = 
+{
+    {AutoModel_Init},
+};
+
+#define GetModulesCount()   (sizeof(system_module_init)/sizeof(module_init_t))    
+
+/* static helper function prototype */
 static void main_PrintArgs(int argc, char *argv[]);
+static void main_InitSystemModules(void);
 
 int main(int argc, char *argv[])
 {
     // main_PrintArgs(argc, argv);
     // AutoModel_ModelState();
 
-    // create basic pthread app
-	pid_t pid;
+    /* create another process */
+	pid_t pid = fork();
+
+    if (0 == pid)
+    {
+        /* child process */
+        
+    }
+    else
+    {
+        /* parent process */
+
+    }
 
 	printf("I'm a child process talking to you from another program, pid = %d\n", getpid());
 	printf("I'm a child process talking to you from another program, my parent pid = %d\n", getppid());
@@ -41,4 +69,33 @@ static void main_PrintArgs(int argc, char *argv[])
     {
         printf("Argument list is empty!\n");
     }
+}
+
+/**
+ * Init system modules, whcich is in declarated in modules array.
+ */
+static void main_InitSystemModules(void)
+{
+    //Parent code (Before all child processes start)
+
+
+    for (int i = 0; i < GetModulesCount(); i++)
+    {
+        pid_t child_pid = fork();
+        if (0 == child_pid) 
+        {
+            /* child code */
+            system_module_init[i].module_init_func();
+
+            // it is child process now and before that all threads within process should be joined and after that process can exit(0) (with code zero = mean success)
+            exit(EXIT_SUCCESS);
+        }
+    }
+    
+    int status = 0;
+    pid_t wpid;
+    while ((wpid = wait(&status)) > 0); // this way, the father waits for all the child processes 
+
+    //Parent code (After all child processes end)
+
 }
