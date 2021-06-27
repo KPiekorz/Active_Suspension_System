adb78959218c6e49f0733ab2c5fbd4043ee5b768
Krzysztof <kpiekorz919@protonmail.com>
2021-06-27 13:15:08 +0200

    gui

--- gui/gui.c
+++ gui/gui.c
@@ -20,16 +20,15 @@ void Gui_RunGui(void)
     Py_Initialize();
 
     errno = 0;
+    system("chmod +x gui_plot.py");
     FILE* PScriptFile = fopen("gui_plot.py", "r");
 
     if (PScriptFile == NULL) 
     {
-        printf("Error %d \n\r", errno);
-        printf("It's null\n\r");
+        printf("File to open py gui, Error: %d \n\r", errno);
     }
     else
     {
-        printf("working\r\n");
         PyRun_SimpleFile(PScriptFile, "gui_plot.py");
         fclose(PScriptFile);
     }
