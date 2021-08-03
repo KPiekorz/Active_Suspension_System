68d51f774d9d2a2fb98de72993d9b8499691d56b
Krzysztof <kpiekorz919@protonmail.com>
2021-08-03 13:33:10 +0200

    remove useless code and add plot update

--- gui/gui_plot.py
+++ gui/gui_plot.py
@@ -7,6 +7,7 @@ import matplotlib
 import os
 import socket
 import sys
+import random
 
 matplotlib.use('Qt5Agg')
 
@@ -17,67 +18,6 @@ class MplCanvas(FigureCanvasQTAgg):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         super(MplCanvas, self).__init__(fig)
-
-
-# class UDPWorker(QtCore.QObject):
-#     dataChanged = QtCore.pyqtSignal(str)
-
-#     def __init__(self, parent=None):
-#         super(UDPWorker, self).__init__(parent)
-#         self.server_start = False
-
-#     @QtCore.pyqtSlot()
-#     def start(self):
-#         self.server_start = True
-#         ip = '' # INADDR_ANY
-#         port = 1100
-#         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#         self.sock.bind((ip, port))
-#         self.process()
-
-#     def process(self):
-
-#         while (1):
-#             print("Hello from process!")
-
-#         # while self.server_start:
-#         #     data, addr = self.sock.recvfrom(1024)
-#         #     print("Data udp data received: " + data)
-
-#             # self.dataChanged.emit(str(data))
-
-
-# class MainWindow(QtWidgets.QMainWindow):
-#     started = QtCore.pyqtSignal()
-
-#     def __init__(self, *args, **kwargs):
-#         super(MainWindow, self).__init__(*args, **kwargs)
-
-#         sc = MplCanvas(self, width=5, height=4, dpi=100)
-#         sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
-
-#         # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-#         toolbar = NavigationToolbar(sc, self)
-
-#         layout = QtWidgets.QVBoxLayout()
-#         layout.addWidget(toolbar)
-#         layout.addWidget(sc)
-
-#         # Create a placeholder widget to hold our toolbar and canvas.
-#         widget = QtWidgets.QWidget()
-#         widget.setLayout(layout)
-#         self.setCentralWidget(widget)
-
-#         self.show()
-
-#     @QtCore.pyqtSlot(str)
-#     def addItem(self, text):
-#         # self.lst.insertItem(0, text)
-#         print("Data udp data received: " + text)
-
-
-
-
 
 class UDPServer(QtCore.QObject):
 
@@ -137,15 +77,13 @@ class GUI(QtWidgets.QMainWindow):
         button_start_serial_reader.clicked.connect(self.signal_start_serial_reader)
 
         # Set plot paramaters
-        plot_control = MplCanvas(self, width=5, height=4, dpi=100)
-        plot_control.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
-
-        plot_model = MplCanvas(self, width=5, height=4, dpi=100)
-        plot_model.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+        self.plot_control = MplCanvas(self, width=5, height=4, dpi=100)
+        self.plot_model = MplCanvas(self, width=5, height=4, dpi=100)
+        self.update_plot()
 
         # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-        toolbar_control = NavigationToolbar(plot_control, self)
-        toolbar_model = NavigationToolbar(plot_model, self)
+        toolbar_control = NavigationToolbar(self.plot_control, self)
+        toolbar_model = NavigationToolbar(self.plot_model, self)
 
         # Setup box layout
         box_layout = QtWidgets.QVBoxLayout()
@@ -153,9 +91,9 @@ class GUI(QtWidgets.QMainWindow):
         box_layout.addWidget(button_start_udp_server)
         box_layout.addWidget(button_start_serial_reader)
         box_layout.addWidget(toolbar_control)
-        box_layout.addWidget(plot_control)
+        box_layout.addWidget(self.plot_control)
         box_layout.addWidget(toolbar_model)
-        box_layout.addWidget(plot_model)
+        box_layout.addWidget(self.plot_model)
 
         # Create a placeholder widget to hold our toolbar and canvas.
         widget = QtWidgets.QWidget()
@@ -164,6 +102,27 @@ class GUI(QtWidgets.QMainWindow):
 
         # Show gui
         self.show()
+
+        # Setup timers to update draw the plot
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(500)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+    def update_plot(self):
+        # clear plot
+        self.plot_control.axes.cla()
+        # set new plot axes values
+        self.plot_control.axes.plot([0, 1, 2, 3, 4], [10, 1, random.randint(0, 10), 3, 40])
+        # Trigger the canvas to update and redraw.
+        self.plot_control.draw()
+
+        # clear plot
+        self.plot_model.axes.cla()
+        # set new plot axes values
+        self.plot_model.axes.plot([0, 1, 2, 3, 4], [10, 1, random.randint(0, 10), 3, random.randint(0, 10)])
+        # Trigger the canvas to update and redraw.
+        self.plot_model.draw()
 
 if __name__ == '__main__':
 
@@ -184,53 +143,3 @@ if __name__ == '__main__':
     gui.signal_start_serial_reader.connect(serial_reader.read_serial)
 
     sys.exit(app.exec_())
-
-
-# if __name__ == '__main__':
-#     app = QtWidgets.QApplication(sys.argv)
-#     w = MainWindow()
-
-#     udp_worker = UDPWorker()
-#     thread = QtCore.QThread()
-#     thread.start()
-#     udp_worker.moveToThread(thread)
-#     w.started.connect(udp_worker.start)
-#     udp_worker.dataChanged.connect(w.addItem)
-
-#     sys.exit(app.exec_())
-
-
-'''
-class UDPWidget(QtWidgets.QWidget):
-    started = QtCore.pyqtSignal()
-
-    def __init__(self, parent=None):
-        super(UDPWidget, self).__init__(parent)
-        btn = QtWidgets.QPushButton("Click Me")
-        btn.clicked.connect(self.started)
-        self.lst = QtWidgets.QListWidget()
-
-        lay = QtWidgets.QVBoxLayout(self)
-        lay.addWidget(QtWidgets.QLabel("udp receiver"))
-        lay.addWidget(btn)
-        lay.addWidget(self.lst)
-
-        self.setWindowTitle("udp receive")
-
-    @QtCore.pyqtSlot(str)
-    def addItem(self, text):
-        self.lst.insertItem(0, text)
-
-if __name__ == '__main__':
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    w = UDPWidget()
-    worker = UDPWorker()
-    thread = QtCore.QThread()
-    thread.start()
-    worker.moveToThread(thread)
-    w.started.connect(worker.start)
-    worker.dataChanged.connect(w.addItem)
-    w.show()
-    sys.exit(app.exec_())
-'''
